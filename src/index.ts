e29c06a2839462835eeda16141f11d8b2bfd397b
Brian Patrick <bjpatric@us.ibm.com>
2023-07-26 15:15:58 -0500

    build: fix build

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-export {
-  Header,
-  HeaderNav,
-  HeaderLink,
-  HeaderFlyouts,
-  HeaderFlyout,
-  HeaderAvatar,
-} from './components/global/header';
-export { SideNav, SideNavLink } from './components/global/sideNav';
-export { Button } from './components/reusable/button';
-export { Icon } from './components/reusable/icon';
-export { Footer, FooterNav, FooterNavLink } from './components/global/footer';
+export { Button } from './components/button';
+export { Icon } from './components/icon';
