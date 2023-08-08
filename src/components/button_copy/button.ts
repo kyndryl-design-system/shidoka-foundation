0d88b77acb58ea37156b1619f3377fd4259c3b00
peterpham-kyndryl <peter.pham@kyndryl.com>
2023-08-08 08:10:29 -0500

    feat(button): porting button component from kyndryl storybook

---
+++ src/components/button_copy/button.ts
@@ -0,0 +1,72 @@
+/**
+ * Copyright Kyndryl, Inc. 2022
+ */
+
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { PREFIX_CLASS, PREFIX_TAG } from '../../global/settings/settings';
+import { ICON_IDS } from '../../global/defs/iconIds';
+import { LINK_TARGETS } from '../../global/defs/linkTargets';
+// import { THEMES } from '../../global/defs/themes';
+import { BUTTON_SIZES, BUTTON_TYPES, BUTTON_ICON_POSITION } from './defs';
+import '../icon/icon';
+import stylesheet from './button.scss';
+
+/**
+ * Kyndryl branded L0
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement(`${PREFIX_TAG}-button`)
+export class Button extends LitElement {
+  static styles = [stylesheet];
+
+  @property({ type: String }) description;
+  @property({ type: String }) href;
+  @property() target: LINK_TARGETS;
+  @property() type: BUTTON_TYPES;
+  @property() size: BUTTON_SIZES;
+  @property() icon: ICON_IDS;
+  @property() iconPosition: BUTTON_ICON_POSITION;
+  // @property() theme: THEMES;
+
+  render() {
+    const classes = classMap({
+      [`${PREFIX_CLASS}-btn-primary`]:
+        this.type === BUTTON_TYPES.PRIMARY || !this.type,
+      [`${PREFIX_CLASS}-btn-secondary`]: this.type === BUTTON_TYPES.SECONDARY,
+      [`${PREFIX_CLASS}-btn-text`]: this.type === BUTTON_TYPES.TEXT,
+      [`${PREFIX_CLASS}-btn-lg`]: this.size === BUTTON_SIZES.LARGE,
+      [`${PREFIX_CLASS}-btn-sm`]: this.size === BUTTON_SIZES.SMALL,
+      [`${PREFIX_CLASS}-btn-icon-${this.iconPosition}`]: this.iconPosition,
+      // [`${PREFIX_CLASS_THEME}-${this.theme}`]: this.theme,
+    });
+    if (!this.icon) {
+      return html`<a
+        id="button"
+        href=${this.href}
+        target=${this.target}
+        class=${classes}
+        aria-label=${ifDefined(this.description ? this.description : undefined)}
+      >
+        <slot></slot>
+      </a>`;
+    } else {
+      return html`<a
+        id="button"
+        href=${this.href}
+        target=${this.target}
+        class=${classes}
+        aria-label=${ifDefined(this.description ? this.description : undefined)}
+      >
+        <slot></slot>
+        <span class="${PREFIX_CLASS}-btn--icon">
+          <kd-icon icon=${this.icon}></kd-icon>
+        </span>
+      </a>`;
+    }
+  }
+}
---
+++ src/global/mixins/global.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright Kyndryl, Inc. 2022
+ */
+ 
+import { APP_DOMAIN } from '../settings/settings';
+
+const url = require('url');
+
+/**
+ * Generic URL formatter function.
+ *
+ * @param {*} path
+ *   Path for formatted URL.
+ * @param {*} options
+ *   NodeJS URL object.
+ * @returns Formatted URL string.
+ */
+export const formatUrl = (path, options = {}) => {
+  const urlObject = {
+    host: APP_DOMAIN,
+    pathname: path || null,
+    ...options,
+  };
+  const urlStr = url.format(urlObject);
+  return urlStr.toString();
+};
+
+/**
+ * Convert an object to an array of only its values.
+ * Used when importing enums in component stories for populating argType dropdowns.
+ * @param {*} options
+ *  imported enums object
+ */
+export function createOptionsArray(options = {}) {
+  const optionsArray = [];
+
+  Object.keys(options).map((key) => {
+    optionsArray.push(options[key]);
+  });
+
+  return optionsArray;
+}
