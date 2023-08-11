1cc5308c69fba4d857429ed088c7034aaeca2af5
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2023-08-11 15:01:27 +0530

    feat: Added primary and secondary classes with inline and standalone link variant

--- src/components/link/Link.stories.js
+++ src/components/link/Link.stories.js
@@ -1,17 +1,63 @@
 import { html } from 'lit';
 import './link';
+import arrowRightIcon from '@carbon/icons/es/chevron--right/16';
 
 export default {
   title: 'Components/Link',
   component: 'kd-link',
-  argTypes: {},
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg'],
+    },
+    kind: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary'],
+    },
+  },
 };
 
 const args = {
   href: '',
+  size: 'md',
+  target: '_self',
+  kind: 'primary',
 };
+
+const linkIconArgs = { ...args, sizeOverride: 16 };
 
 export const Link = {
   args: args,
-  render: (args) => html` <kd-link href=${args.href}> Link1 </kd-link> `,
+  render: (args) =>
+    html`
+      <kd-link
+        inline
+        href=${args.href}
+        size=${args.size}
+        target=${args.target}
+        kind=${args.kind}
+      >
+        Link1
+      </kd-link>
+    `,
+};
+
+export const LinkWithIcon = {
+  args: linkIconArgs,
+  render: (args) => html`
+    <kd-link
+      href=${args.href}
+      size=${args.size}
+      target=${args.target}
+      kind=${args.kind}
+    >
+      Link1
+      <span style="display: inline-flex; align-self: center;">
+        <kyn-icon
+          .icon=${arrowRightIcon}
+          sizeOverride=${args.size === 'lg' ? '20' : args.sizeOverride}
+        ></kyn-icon>
+      </span>
+    </kd-link>
+  `,
 };
--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -1,5 +1,8 @@
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import {LINK_TYPES, LINK_TARGETS, LINK_SIZES } from './defs';
+
 import LinkStyles from './link.scss';
 
 /**
@@ -9,48 +12,79 @@ import LinkStyles from './link.scss';
  */
 
 @customElement('kd-link')
+
 export class Link extends LitElement {
-  static override styles = LinkStyles;  
-
-  /** Defines a target attribute for where to load the URL. Possible options include "_self" (deafult), "_blank", "_parent", "_top" */
-  @property({ type: String })
-  target = '_self' as const;
-  
-  /** Defines a relationship between a linked resource and the document. An empty string (default) means no particular relationship */
-  @property({ type: String })
-  rel = '';
-
-  /** Link url. */
-  @property({ type: String })
-  href = '';
-
-  override render() {
-    return html`
-      <a
-        class="text-link"
-        target=${this.target}
-        role='link'
-        rel=${this.rel}
-        href=${this.href ? this.href : 'javascript:void(0)'}
-        @click=${(e: Event) => this.handleClick(e)}
-      >
-        <slot></slot>
-      </a>
-    `;
-  }
-
-  private handleClick(e: Event) {
-    console.log('called');
+    static override styles = LinkStyles; 
     
-    const event = new CustomEvent('on-click', {
-      detail: { origEvent: e },
-    });
-    this.dispatchEvent(event);
-  }
-}
+    /** 
+     * @description Link url. 
+     * */
+    @property({ type: String })
+    href = '';
+     /** 
+     * @description Link target. 
+     * */
+    @property({type: String}) target = LINK_TARGETS.SELF;
 
+    /** 
+     * @description The Link size. 
+     * */
+    @property({ type: String }) size = LINK_SIZES.MEDIUM; // default : App (primary) 
+     /** 
+     * @description The Link type. 
+     * */
+    @property() kind = LINK_TYPES.PRIMARY;
+
+    /** 
+     * @description 
+     * Defines a relationship between a linked resource and the document. An empty string (default) means no particular relationship 
+     * */
+    @property({ type: String })
+    rel = '';
+    /** 
+     * @description Disables the link. 
+    */
+    @property({ type: Boolean })
+    disabled = false;
+
+   // @property({type: String}) variant = LINK_VARIANTS.INLINE;
+   @property ({type: Boolean})
+   inline = false;
+
+    override render(){
+        const classes = classMap({
+            ['kd-link-text-primary']: this.kind === LINK_TYPES.PRIMARY || !this.kind,
+            ['kd-link-text-secondary']: this.kind === LINK_TYPES.SECONDARY,
+            ['kd-link-text-md']: this.size === LINK_SIZES.MEDIUM,
+            ['kd-link-text-lg']: this.size === LINK_SIZES.LARGE,
+            ['kd-link-text-sm']: this.size === LINK_SIZES.SMALL,
+            ['kd-link-text-inline']: this.inline,
+            ['kd-link-text-standalone']: !this.inline,
+            ['kd-link-text-disabled']: this.disabled === true
+        });
+
+        return html`
+        <a
+          class="kd-link-text ${classes}"
+          target=${this.target}
+          role='link'
+          rel=${this.rel}
+          href=${this.href ? this.href : 'javascript:void(0)'}
+          @click=${(e: Event) => this.handleClick(e)}
+        >
+          <slot></slot>
+        </a>`;
+    }
+
+    private handleClick(e: Event) {
+        const event = new CustomEvent('on-click', {
+          detail: { origEvent: e },
+        });
+        this.dispatchEvent(event);
+    }
+}
 declare global {
-  interface HTMLElementTagNameMap {
-    'kd-link': Link;
-  }
+    interface HTMLElementTagNameMap {
+      'kd-link': Link;
+    }
 }
