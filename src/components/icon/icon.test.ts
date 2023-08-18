997b082794f0d9445f7d919f52bdaae10cd1adb7
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2023-08-18 11:16:12 +0530

    test: test file for link component

--- src/components/icon/icon.test.ts
+++ src/components/icon/icon.test.ts
@@ -1,10 +1,10 @@
-import { Link } from '../../../dist/index';
+import { Icon } from '../../../dist/index';
 
 import { assert } from '@open-wc/testing';
 
-suite('kd-link', () => {
+suite('kd-icon', () => {
   test('is defined', () => {
-    const el = document.createElement('kd-link');
-    assert.instanceOf(el, Link);
+    const el = document.createElement('kd-icon');
+    assert.instanceOf(el, Icon);
   });
 });
