2bb6fb0b592a81425298d2004faa024d11975414
Diana Nikita <diananikita@Dianas-MacBook-Pro-2.local>
2023-08-22 19:51:05 +0530

    updated spacer elements

---
+++ src/stories/Spacing.stories.js
@@ -0,0 +1,37 @@
+import { html } from 'lit-html';
+
+
+export const Spacing = () => html`
+<div>
+<!-- Your content using spacing utility classes -->
+<div class="kd-spacing__padding-top-8">kd-spacing__padding-top-8</div>
+<div class="kd-spacing__padding-bottom-8">kd-spacing__padding-bottom-8</div>
+<div class="kd-spacing__padding-left-8">kd-spacing__padding-left-8</div>
+<div class="kd-spacing__padding-right-8">kd-spacing__padding-right-8</div>
+<div class="kd-spacing__margin-right-8">kd-spacing__margin-right-8</div>
+<div class="kd-spacing__margin-left-8">kd-spacing__margin-left-8</div>
+<div class="kd-spacing__padding-top-32">kd-spacing__padding-top-32</div>
+<div class="kd-spacing__margin-0">Hello! </div>
+<div class="kd-spacing__padding-0">Hello! (padding-0)</div>
+<div class="kd-spacing__padding-2">Hello! (padding-2)</div>
+<div class="kd-spacing__padding-4">Hello! (padding-4)</div>
+<div class="kd-spacing__padding-8">Hello! (padding-8)</div>
+<div class="kd-spacing__padding-12">Hello! (padding-12)</div>
+<div class="kd-spacing__padding-16">Hello! (padding-16)</div>
+<div class="kd-spacing__padding-24">Hello! (padding-24)</div>
+<div class="kd-spacing__padding-32">Hello! (padding-32)</div>
+<div class="kd-spacing__padding-40">Hello! (padding-40)</div>
+<div class="kd-spacing__padding-48">Hello! (padding-48)</div>
+<div class="kd-spacing__padding-64">Hello! (padding-64)</div>
+<div class="kd-spacing__padding-72">Hello! (padding-72)</div>
+<div class="kd-spacing__padding-80">Hello! (padding-80)</div>
+<div class="kd-spacing__padding-96">Hello! (padding-96)</div>
+<div class="kd-spacing__padding-144">Hello! (padding-144)</div>
+<div class="kd-spacing__padding-160">Hello! (padding-160)</div>
+</div>
+`;
+
+
+export default {
+title: 'Foundation/Spacing',
+};
