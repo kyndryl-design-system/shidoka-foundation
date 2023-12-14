0478e3f11350beaba1a679f98fa4864bb3b9367d
Zak Eddington <zak.eddington@wearepop.com>
2023-12-14 14:35:40 -0800

    feat(visibility-utils): add documentation and mixin for each breakpoint

---
+++ src/stories/Visibility.stories.js
@@ -0,0 +1,61 @@
+import { html } from 'lit';
+import { classMap } from 'lit/directives/class-map.js';
+
+export default {
+  title: 'Foundation/Visibility',
+  decorators: [
+    (story) =>
+      html`
+        <style>
+          .storybook-visibility-example {
+            align-items: center;
+            background: var(--kd-color-cloud-10);
+            display: flex;
+            margin: 16px;
+            min-height: 4em;
+            padding: 16px;
+          }
+        </style>
+        ${story()}
+      `,
+  ],
+};
+
+const args = {};
+
+export const Visibility = {
+  args: args,
+  render: (args) => {
+    return html`
+      <div class="storybook-visibility-example">
+        This block includes text for screen readers only.
+        <div class="kd-visibility--sr-only">screen reader only content</div>
+      </div>
+      <div class="storybook-visibility-example">
+        <div class="kd-visibility--hidden-sm">
+          Text hidden at Small (sm) breakpoint.
+        </div>
+      </div>
+      <div class="storybook-visibility-example">
+        <div class="kd-visibility--hidden-md">
+          Text hidden at Medium (md) breakpoint.
+        </div>
+      </div>
+      <div class="storybook-visibility-example">
+        <div class="kd-visibility--hidden-lg">
+          Text hidden at Large (lg) breakpoint.
+        </div>
+      </div>
+      <div class="storybook-visibility-example">
+        <div class="kd-visibility--hidden-xl">
+          Text hidden at X-Large (xl) breakpoint.
+        </div>
+      </div>
+      <div class="storybook-visibility-example">
+        <div class="kd-visibility--hidden-max">
+          Text hidden at Max (max) breakpoint.
+        </div>
+      </div>
+    `;
+  },
+};
