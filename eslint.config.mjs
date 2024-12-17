648380632788de983e38d1492d3e91862755a70c
Brian Patrick <brian.patrick3@kyndryl.com>
2024-12-17 13:13:38 -0600

    chore: migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,75 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "node_modules/*",
+        "**/custom-elements.json",
+        "**/webpack.config.js",
+        "**/rollup.config.js",
+        "tests/*",
+        "dist/*",
+        "coverage/*",
+        "storybook-static/*",
+    ],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-prototype-builtins": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            argsIgnorePattern: "^_",
+        }],
+    },
+}, {
+    files: ["**/web-test-runner.config.js"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+}, {
+    files: ["**/*_test.ts", "**/custom_typings/*.d.ts"],
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+}];
