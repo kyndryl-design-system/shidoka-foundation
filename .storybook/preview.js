59cec11531bad7820a668b8b2631a9665811a9e7
Brian Patrick <brian.patrick3@kyndryl.com>
2025-11-10 08:48:41 -0600

    chore: storybook 10 upgrade
    Signed-off-by: Brian Patrick <brian.patrick3@kyndryl.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -35,10 +35,10 @@ export default {
       },
     },
 
-    backgrounds: { disable: true },
+    backgrounds: { disabled: true },
 
     viewport: {
-      viewports: {
+      options: {
         ...BREAKPOINT_VIEWPORTS,
         ...INITIAL_VIEWPORTS,
       },
